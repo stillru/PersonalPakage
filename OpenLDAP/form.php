7d24615450632313dfe5a5224d96bb88ef288e1b
Steve Illichevsky <still.ru@gmail.com>
2010-11-22 12:15:02 +0300

    Инициализация папки для скрипта OpenLDAP Скрипт генерирует файл для загрузки в OpenLDAP 	new file:   OpenLDAP/README 	new file:   OpenLDAP/form.php 	new file:   OpenLDAP/form2.php

---
+++ OpenLDAP/form.php
@@ -0,0 +1,105 @@
+<html><!-- DO NOT EDIT YOUR FORM HERE, PLEASE LOG IN AND EDIT AT FREEDBACK.COM -->
+<form method="post" action="form2.php" accept-charset="UTF-8">
+	<table cellspacing="5" cellpadding="5" border="0">
+		<tr>
+			<td valign="top">
+				<strong>Имя:</strong>
+			</td>
+			<td valign="top">
+				<input type="text" name="name" id="name" size="40" value="" />
+				
+			</td>
+		</tr>
+		<tr>
+			<td valign="top">
+				<strong>Фамилия</strong>
+			</td>
+			<td valign="top">
+				<input type="text" name="second" id="second" size="40" value="" />
+				
+			</td>
+		</tr>
+		<tr>
+			<td valign="top">
+				<strong>Отчество</strong>
+			</td>
+			<td valign="top">
+				<input type="text" name="last" id="last" size="40" value="" />
+				
+			</td>
+		</tr>
+		<tr>
+			<td valign="top">
+				<strong>Организация</strong>
+			</td>
+			<td valign="top">
+				<input type="text" name="org" id="org" size="40" value="Мегалекс" />
+				
+			</td>
+		</tr>
+		<tr>
+			<td valign="top">
+				<strong>Внутренний номер</strong>
+			</td>
+			<td valign="top">
+				<input type="text" name="tel" id="tel" size="40" value="" />
+				
+			</td>
+		</tr>
+		<tr>
+			<td valign="top">
+				<strong>Почтовый адрес</strong>
+			</td>
+			<td valign="top">
+				<input type="text" name="mail" id="mail" size="40" value="" />
+				
+			</td>
+		</tr>
+		<tr>
+                        <td valign="top">
+                                <strong>Подразделение</strong>
+                        </td>
+                        <td valign="top">
+                                <input type="text" name="dep" id="dep" size="40" value="" />
+
+                        </td>
+                </tr>
+                <tr>
+                        <td valign="top">
+                                <strong>Название должности</strong>
+                        </td>
+                        <td valign="top">
+                                <input type="text" name="title" id="title" size="40" value="" />
+
+                        </td>
+                </tr>
+
+		<tr>
+                        <td valign="top">
+                                <strong>Адрес</strong>
+                        </td>
+                        <td valign="top">
+                                <input type="text" name="addres" id="addres" size="40" value="Сибирский проезд 2 корп.9" />
+
+                        </td>
+                </tr>
+		<tr>
+                        <td valign="top">
+                                <strong>Пароль:</strong>
+                        </td>
+                        <td valign="top">
+                                <input type="password" name="pass" id="pass" size="40" value="" />
+
+                        </td>
+                </tr>
+
+		<tr>
+			<td colspan="2" align="center">
+				<input type="submit" value=" Submit Form " />
+			</td>
+		</tr>
+	</table>
+</form>
+<br><center><font face="Arial, Helvetica" size="1"><b>
+</b></font></center>
+</html><!-- End Freedback Form -->
---
+++ OpenLDAP/form2.php
@@ -0,0 +1,78 @@
+<?php
+$name = $_POST['name'];
+$second = $_POST['second'];
+$last = $_POST['last'];
+$dep = $_POST['dep'];
+$org = $_POST['org'];
+$mail = $_POST['mail'];
+$tel = $_POST['tel'];
+$addres = $_POST['addres'];
+$pass = $_POST['pass'];
+$title = $_POST['title'];
+###################### Set up the following variables ######################
+# #
+$filename = "mega-lex.ru.ldif"; #CHMOD to 666
+$forward = 0; # redirect? 1 : yes || 0 : no
+$location = "thankyou.htm"; #set page to redirect to, if 1 is above
+# #
+##################### No need to edit below this line ######################
+
+
+## mail message ##
+
+$msg .= "dn: cn=$second $name, ou=OFFICE, o=MegaLex, l=Moscow, o=bd, dc=mega-lex, dc=ru" . "\n";
+$msg .= "objectClass: top" . "\n";
+$msg .= "objectClass: person" . "\n";
+$msg .= "objectClass: organizationalPerson" . "\n";
+$msg .= "objectClass: inetOrgPerson" . "\n";
+$msg .= "objectClass: posixAccount" . "\n";
+$msg .= "objectClass: inetLocalMailRecipient" . "\n";
+$msg .= "objectClass: OpenLDAPperson" . "\n";
+$msg .= "company: MEGALEX_MOS_OFFICE" . "\n";
+$msg .= "o: MEGALEX_MOS_OFFICE" . "\n";
+$msg .= "homeDirectory: /mail/mega-lex.ru/victor" . "\n";
+$msg .= "maildrop: Maildir/" . "\n";
+$msg .= "mailAccessValue: OK" . "\n";
+$msg .= "uidNumber: 2000" . "\n";
+$msg .= "gidNumber: 2000" . "\n";
+$msg .= "mailAliasKey: all@mega-lex.ru" . "\n";
+$msg .= "cn: $second "."$name" . "\n";
+$msg .= "sn: $second" . "\n";
+$msg .= "givenName: $name" . "\n";
+$msg .= "initials: $last" . "\n";
+$msg .= "title: $title" . "\n";
+$msg .= "telephoneNumber: $tel" . "\n";
+$msg .= "postalAddress: $addres" . "\n";
+$msg .= "ou: $dep" . "\n";
+$msg .= "department: $dep" . "\n";
+$msg .= "mail: $mail" . "\n";
+$msg .= "mailAliasValue: $mail" . "\n";
+$msg .= "uid: $mail" . "\n";
+$msg .= "mailquota: 1000000000" . "\n";
+
+$userpassword = "{SHA}" . base64_encode( pack( "H*", sha1( $pass ) ) ); 
+
+
+$msg .= "userPassword:$userpassword" . "\n";
+$msg .= "\n\n";
+
+$fp = fopen ($filename, "a"); # w = write to the file only, create file if it does not exist, discard existing contents
+if ($fp) {
+fwrite ($fp, $msg);
+fclose ($fp);
+}
+else {
+$forward = 2;
+}
+
+if ($forward == 1) {
+header ("Location:$location");
+}
+else if ($forward == 0) {
+echo ("Thank you for submitting our form. We will get back to you as soon as possible.");
+}
+else {
+"Error processing form. Please contact the webmaster";
+};
+
+?>
