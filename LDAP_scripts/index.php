c9207f65715273d0b8aab769e78b99542220cb0d
Steve Illichevsky <illichevski.s@mega-lex.ru>
2010-12-22 18:55:54 +0300

    Clean tails ^=-)))\

--- LDAP_scripts/index.php
+++ LDAP_scripts/index.php
@@ -4,32 +4,6 @@
 <meta name="description" content="Only personal code">
 </head>
 <body>
-<?php
-$pageURL = 'http';
-if ($_SERVER["HTTPS"] == "on") {        
-    $pageURL .= "s";    
-}       
-$pageURL .= "://";      
-if ($_SERVER["SERVER_PORT"] != "80") {  
-    $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];    
-} else {        
-    $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];        
-}
-
-session_name('MYAPP');  
-session_start();        
-if (!isset($_SESSION["user"])) {        
-    include("LdapAuth.inc.php");        
-    $ldap=new LdapAuth();       
-    $ldap->setSessionAttr("user","name");        
-    $ldap->setSessionName("MYAPP");     
-    $ldap->setRedirectPage($pageURL); //page where we get redirected after login (in this case self)
-    include("LdapStandalonePageProtector.inc.php");
-}       
-else { 
-  echo "Logged In As: ".$_SESSION["user"]."</hr>";
-  //paste here the old page code (or write the new page to protect)
-?>
 
 <form method="post" action="form2.php">
 	<table cellspacing="5" cellpadding="5" border="0">
@@ -136,6 +110,3 @@ else {
 <br><center><font face="Arial, Helvetica" size="1"><b>
 </b></font></center>
 </body></html>
-<?php
-}
-?>
