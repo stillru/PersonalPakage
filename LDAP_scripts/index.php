a168c48e58ac7c7fdcf182a732ac163d32d0ef01
stillru <still.ru@gmail.com>
2010-12-22 19:44:15 +0300

    Add binding to LDAP

--- LDAP_scripts/index.php
+++ LDAP_scripts/index.php
@@ -5,6 +5,47 @@
 </head>
 <body>
 
+<?php
+$ldapconfig['host'] = '192.168.1.201';
+$ldapconfig['port'] = 389;
+$ldapconfig['basedn'] = 'dc=mega-lex,dc=ru';
+$password="Keeping-Cooler";
+$user=admin;
+$ds=ldap_connect($ldapconfig['host'], $ldapconfig['port']);
+$dn="cn=". $user .",". $ldapconfig['basedn'];
+if ($bind=ldap_bind($ds,$dn,$password)) {
+  echo("Login correct");
+} else {
+
+  echo("Unable to bind to server.</br>");
+
+  echo("msg:'".ldap_error($ds)."'</br>");
+	//check if the message isn't: Can't contact LDAP server :)
+  	//if it say something about a cn or user then you are trying with the wrong $dn pattern i found this by looking at OpenLDAP source code :)
+  	//we can figure out the right pattern by searching the user tree
+  	//remember to turn on the anonymous search on the ldap server
+  if ($bind=ldap_bind($ds)) {
+
+    $filter = "(cn=*)";
+
+    if (!($search=@ldap_search($ds, $ldapconfig['basedn'], $filter))) {
+      echo("Unable to search ldap server<br>");
+      echo("msg:'".ldap_error($ds)."'</br>");#check the message again
+    } else {
+      $number_returned = ldap_count_entries($ds,$search);
+      $info = ldap_get_entries($ds, $search);
+      echo "The number of entries returned is ". $number_returned."<p>";
+      for ($i=0; $i<$info["count"]; $i++) {
+
+        var_dump($info[$i]);//look for your user account in this pile of junk and apply the whole pattern where you build $dn to match exactly the ldap tree entry
+      }
+    }
+  } else {
+    echo("Unable to bind anonymously<br>");
+    echo("msg:".ldap_error($ds)."<br>");
+  }
+}
+?>
 <form method="post" action="form2.php">
 	<table cellspacing="5" cellpadding="5" border="0">
 		<tr>
