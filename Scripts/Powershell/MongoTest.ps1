63dee6e7a038ca1e362561deed0e2456e3b8ba02
StIll <still.ru@gmail.com>
2011-03-10 02:37:41 +0300

    Trying to play with NoSQL Database Need PowerShell Driver ^(

---
+++ Scripts/Powershell/MongoTest.ps1
@@ -0,0 +1,18 @@
+# We assume that the Driver is installed via the MSI.
+#[string] $mongoDriverPath = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\.NETFramework\AssemblyFolders\MongoDB CSharpDriver 0.11").'(default)';
+#Add-Type -Path "$($mongoDriverPath)\MongoDB.Bson.dll";
+add-type -Path .\MongoDB.Driver.dll
+
+$mongo=new-object MongoDB.Driver.Mongo
+$mongo.connect()
+$db=$mongo.GetDataBase("movieReviews")
+$movies=$db.GetCollection('Movies')
+
+$movie=new-object MongoDB.Driver.document
+$movie['title']='Star Wars'
+$movie['releaseDate']=Get-Date
+$movies.Insert($movie)
+
+$spec=new-object MongoDB.Driver.document
+$spec['title']='Star Wars'
+$movies.FindOne($spec)
