7503298d4cc14e38f533ed3b8120c92009d54672
StIll <still.ru@gmail.com>
2011-04-28 18:44:37 +0400

    Add Script for creating local users

--- Scripts/Powershell/Work/Add-UserCSV.ps1
+++ Scripts/Powershell/Work/Add-UserCSV.ps1
@@ -1,24 +1,36 @@
-import-csv ".\userlist2.csv" | foreach-object {
-[string]$ConnectionString = "WinNT://SEVER,computer" 
-        $ADSI = [adsi]$ConnecionString 
+import-csv ".\u.csv" | foreach-object {
+[string]$ConnectionString = "WinNT://$env:computername,computer" 
+        $ADSI = [adsi]$ConnectionString 
         $User = $ADSI.Create("user",$_.Name) 
         $User.SetPassword($_.Password) 
         $User.put("description",$_.Description)
         $User.SetInfo()
         $User.put("fullname",$_.Full_Name)
         $User.SetInfo()
-
+		$User.put("PasswordExpired",1)
+        $User.SetInfo()
+[string]$fullname = $_.Full_Name
 [string]$group1 = $_.Main_Group
         write-host $_.Name "created..."
         $name = $_.Name
         write-host $group1 "selected..."
-        sleep 10
-        $objOU = [ADSI]"WinNT://SEVER/$group1,group"
-        $objOU.add("WinNT://SEVER/$name")
+        sleep 2
+        $objOU = [ADSI]"WinNT://$env:computername/$group1,group"
+        $objOU.add("WinNT://$env:computername/$name")
 [string]$group2 = $_.Secondary_Group
         write-host $group2 "selected..."
-        sleep 10
-        $objOU = [ADSI]"WinNT://SEVER/$group2,group"
-        $objOU.add("WinNT://SEVER/$name")
-        write-host "User created and add to groups."
+        sleep 2
+        $objOU = [ADSI]"WinNT://$env:computername/$group2,group"
+        $objOU.add("WinNT://$env:computername/$name")
+[string]$group3 = $_.Users
+        write-host $group3 "selected..."
+        sleep 2
+        $objOU = [ADSI]"WinNT://$env:computername/$group3,group"
+        $objOU.add("WinNT://$env:computername/$name")
+        write-host "User $name\$fullname on $env:computername created and add to groups $group1, $group2, $group3."
 }
+#[System.IO.File]::ReadAllText("C:\Dstrib\Scripts\u.csv") | Out-File "C:\Dstrib\Scripts\u-created.csv" -Append -Encoding Unicode
+mv ".\u.csv" ".\u-created.csv[1-100]"
+del ".\u.csv"
+cp ".\Template.csv" ".\u.csv"
+write-host "Job done!"
