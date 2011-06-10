99c580a3db5e38dc6179333f38072a010d42cc7f
still <still@adminpoint.office.mega-lex.local>
2011-06-10 12:01:45 +0400

    Modify mkpassword script. Now script clearin plan password file after showing new generated password

--- Scripts/Bash/mkpasswd.sh
+++ Scripts/Bash/mkpasswd.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 x=`tr -cd [:alnum:] < /dev/urandom | head -c8`
+if [ $1 = ""]; then
+	echo "Need some information about Pepole"
+else
+gpg password.txt.gpg
 echo ${x:0:4}-${x:4:4}"	 $1	"	`date` >> password.txt
 gpg -c password.txt
+echo $x $1 `date`
 rm password.txt
+fi
