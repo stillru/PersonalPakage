0c79577eba7092f2cae56aac70df51cba8ddd583
StIll <still.ru@gmail.com>
2011-06-10 12:10:01 +0400

    Add Serial numbers statment

--- Scripts/Bash/mkpasswd.sh
+++ Scripts/Bash/mkpasswd.sh
@@ -2,9 +2,11 @@
 x=`tr -cd [:alnum:] < /dev/urandom | head -c8`
 if [ $1 = ""]; then
 	echo "Need some information about Pepole"
+elif [ $2 ="" ]; then
+	echo "Need token Serial Number"
 else
 gpg password.txt.gpg
-echo ${x:0:4}-${x:4:4}"	 $1	"	`date` >> password.txt
+echo ${x:0:4}-${x:4:4}"	 $1 $2	"	`date` >> password.txt
 gpg -c password.txt
 echo $x $1 `date`
 rm password.txt
