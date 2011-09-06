e6ca51879e1a6485d80895e0adb9eddc1173e07e
still <still.old@adminpoint.office>
2011-09-06 17:21:02 +0400

    Add script convert.sh for converting files with structure

---
+++ Scripts/Bash/convert.sh
@@ -0,0 +1,10 @@
+echo "Start converting images."
+echo "Step one: Create resized structure"
+find . -type d | cpio -pvdm resized > /dev/null
+echo "Directory structure created..."
+echo "Step two: Converting jpg files to created structure..."
+find * -name '*.jpg' -exec  convert '{}' -resize 1000x1000 resized/'{}'.jpg \;
+echo "...Done."
+echo "Step three: Move resized folder."
+mv -r resized ../resized
+echo "Done! All files resized!"
